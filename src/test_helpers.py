9910f6ea0792e992a40c685d5bf80c001f85c19f
Davit Tsakadze <davittsakadzee@gmail.com>
2024-12-30 01:29:47 +0400

    added text_to_textnodes function

--- src/test_helpers.py
+++ src/test_helpers.py
@@ -1,6 +1,6 @@
 import unittest
 
-from helpers import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from helpers import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextType, TextNode
 
 
@@ -60,6 +60,26 @@ class TestHelpers(unittest.TestCase):
         TextNode(" and ", TextType.TEXT),
         TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
       ],
+    )
+
+  def test_text_to_textnodes(self):
+    nodes = text_to_textnodes(
+      "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+    )
+    self.assertListEqual(
+      [
+        TextNode("This is ", TextType.TEXT),
+        TextNode("text", TextType.BOLD),
+        TextNode(" with an ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word and a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" and an ", TextType.TEXT),
+        TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+        TextNode(" and a ", TextType.TEXT),
+        TextNode("link", TextType.LINK, "https://boot.dev"),
+      ],
+      nodes,
     )
 
 
